87b3c88df036559deddb66e8654f2901a6cf916f
BTubbs200 <btubbs200@gmail.com>
2026-03-31 16:29:01 -0500

    add fontset to CPU init

--- main/src/cpu.rs
+++ main/src/cpu.rs
@@ -1,6 +1,29 @@
+use crate::cpu;
+
+const FONTSET: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
 #[derive(Debug)]
 pub struct Cpu {
     memory: [u8; 4096],
+    display: [u8; 64 * 32],
+
     v_regs: [u8; 16], // 16 8-bit V registers 0-F
     index_reg: u16,
     pc_reg: u16,
@@ -14,6 +37,7 @@ impl Default for Cpu {
     fn default() -> Self {
         Self {
             memory: [0; 4096],
+            display: [0; 64 * 32],
             v_regs: [0; 16],
             index_reg: 0,
             pc_reg: 0x200, // Start at address 512
@@ -26,14 +50,31 @@ impl Default for Cpu {
 }
 
 impl Cpu {
-    fn reset(&mut self) {
-        self.memory = [0; 4096];
-        self.v_regs = [0; 16];
-        self.index_reg = 0;
-        self.pc_reg = 0x200;
-        self.stack = [0; 16];
-        self.sp_reg = 0;
-        self.delayt_reg = 0;
-        self.soundt_reg = 0;
+    pub fn new() -> Self {
+        let mut cpu = Self {
+            ..Default::default()
+        };
+
+        // Store fontset into memory starting at 0x50
+        cpu.memory[0x50..0x50 + FONTSET.len()].copy_from_slice(&FONTSET);
+
+        cpu
+    }
+
+    pub fn load_rom(&mut self, rom_buffer: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+        let rom_size = rom_buffer.len();
+
+        // TODO: custom error
+        if rom_size > (4096 - 512) {
+            return Err(format!(
+                "ROM too large. Maximum of 3584 bytes, current file is {} bytes",
+                rom_size
+            )
+            .into());
+        }
+
+        // Load ROM into memory starting at address 512
+        self.memory[512..512 + rom_size].copy_from_slice(rom_buffer);
+        Ok(())
     }
 }
--- main/src/main.rs
+++ main/src/main.rs
@@ -5,31 +5,36 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
-fn main() {
-    // Parse args, expect ROM file to be provided
-    let buffer = parse_args(env::args().collect());
-    let cpu: Cpu = Default::default();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let rom_buffer = parse_args(env::args().collect());
 
-    // TODO: actually do real stuff with ROM file
-    println!("Read {} bytes\nContents: {:?}", buffer.len(), buffer);
+    let mut cpu = Cpu::new();
+    cpu.load_rom(&rom_buffer)?;
+
+    println!(
+        "Read {} bytes\nROM Contents:\n{:?}",
+        rom_buffer.len(),
+        rom_buffer
+    );
     println!("CPU: {:#?}", cpu);
+
+    Ok(())
 }
 
+// TODO: make sure args[1] is actually a file path
+// TODO: more robust ability to implement multiple args as needed
 fn parse_args(args: Vec<String>) -> Vec<u8> {
     if args.len() != 2 {
-        eprintln!(
-            "Path to file not specified.\nUsage: {} <file_path>",
-            args[0]
-        );
+        eprintln!("Usage: {} <file_path>", args[0]);
         std::process::exit(1);
     }
 
     let path = &args[1];
     let mut file = File::open(&path).expect(&format!("Couldn't open {}", path));
-    let mut buffer = Vec::new();
+    let mut rom_buffer = Vec::new();
 
-    file.read_to_end(&mut buffer)
+    file.read_to_end(&mut rom_buffer)
         .expect("Failed to read file.\n");
 
-    return buffer;
+    rom_buffer
 }
